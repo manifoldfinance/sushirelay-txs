e586326dcca0ce08dd3ba6e62bbee7dd6c55484d
sam bacha <87207334+sibacha@users.noreply.github.com>
2021-07-19 03:44:22 -0700

    inital version

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+  
+import { HardhatUserConfig } from 'hardhat/types';
+
+//import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: '0.8.5' }],
+  },
+  networks: {
+    hardhat: {},
+    // rinkeby: {
+    //   url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [RINKEBY_PRIVATE_KEY],
+    // },
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default config;
